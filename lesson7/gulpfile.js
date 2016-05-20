abb831c80f127adfd37bbf1562a55fbd8346b6f8
ouyangli <278500368@qq.com>
2016-05-20 17:56:18 +0800

    add reload

--- lesson7/gulpfile.js
+++ lesson7/gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var watch = require('gulp-watch');
+//var watch = require('gulp-watch');
 var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync');
 var cssnext = require('cssnext');
 var precss = require('precss');
+var reload = browserSync.reload;
 
 //var info = autoprefixer({ browsers: ['last 3 version'] }).info();
 //console.log(info);
@@ -19,10 +21,22 @@ gulp.task('css',function(){
 		autoprefixer({browsers:['last 2 versions']})
 	];
 	return gulp.src(config.style)
-		.pipe(watch(config.style))
+		//.pipe(watch(config.style))
 		.pipe(postcss(processors))
 		.pipe(gulp.dest('./dest'))
+		.pipe(reload({stream:true}))
 });
+
+gulp.task('server',['css'],function(){
+	browserSync({
+		server:{
+			baseDir:'./'
+		}
+	});
+
+	gulp.watch(config.style,['css']);
+	//gulp.watch("./*").on('change', browserSync.reload);
+})
 
 gulp.task('watch',function(){
 	gulp.watch(config.style,function(){
@@ -30,4 +44,4 @@ gulp.task('watch',function(){
 	})
 })
 
-gulp.task('default',['css'])
+gulp.task('default',['server'])
