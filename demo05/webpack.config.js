03d3ff018c8b46170d8479aa604e6a6dabc7c011
bjtqti <278500368@qq.com>
2017-01-04 18:04:01 +0800

    update chunk

--- demo05/webpack.config.js
+++ demo05/webpack.config.js
@@ -1,14 +1,18 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: ['./main.js','./icon.css'],
   output: {
   	path:'./dist',
-    publicPath:'./dist/',
-    filename: 'bundle.js'
+    //publicPath:'./dist/',
+    filename: 'bundle_[hash:8].js'
   },
   module: {
     loaders:[
     	{ test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
-    	{ test: /\.css$/, loader: 'style-loader!css-loader' },
+    	{ test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') },
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css"),
+  ]
 };
